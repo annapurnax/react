2dd51eb1bef6848de75a763e260c168a9c5debd7
annu <74466018+annapurnax@users.noreply.github.com>
2020-11-24 18:25:53 +0400

    push

---
+++ src/Robo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+ function Robo(){
+    return(
+        <div className='bg-light-green dib br3 ma2 grow'>
+      <h1>background</h1>  
+      <button >enter</button>
+      <input type ="text"></input>
+      <img alt = "pic" src="C:\Users\annap\Desktop\image2.jpg"/>
+      </div>
+   
+    );
+}
+ export default Robo;
